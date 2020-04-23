e6f26c002f77f4ebca1ea9df3481ca55cba4b635
reinatrychadani <trycha67@gmail.com>
2020-04-23 12:31:36 +0700

    Menyelesaikan Modul 11 Firebase Login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from "./root";
+/*import './index.css';
+import App from './App';*/
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Root/>,
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*/
   document.getElementById('root')
 );
 
